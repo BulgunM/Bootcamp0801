2ab36b1f85b1e1f852f0b997c969fad1c7112c13
Bulgun <bulgunya@mail.ru>
2023-01-23 00:28:23 +0300

    add Client-Server intercommunion

---
+++ Client_Server/Client/OurClient.cs
@@ -0,0 +1,30 @@
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class OurClient
+    {
+        private TcpClient client; // переменная для работы с клиентом
+        private StreamWriter sWriter; // поток отправки с Клиента на Сервер
+
+        public OurClient() // portnum(5555) куда именно отправлять с сервера, порт(место встречи) должен быть одинаков для клиента и для сервера 
+        {
+            client = new TcpClient("127.0.0.1", 5555); // 127.0.0.1 ip для своего компьютера, сервер отправляет на наш компьютер
+            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+
+            HandleCommunication();
+        }
+
+        void HandleCommunication()
+        {
+            while (true) // бесконечный цикл, будет все время держать соединение с сервером
+            {
+                Console.Write("> ");
+                string message = Console.ReadLine();
+                sWriter.WriteLine(message); // клиент отправляет на сервер сообщение
+                sWriter.Flush();
+            }
+        }
+    }
+}
